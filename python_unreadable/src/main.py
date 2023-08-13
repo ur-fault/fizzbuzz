0a406db144d5b9f9da54398a0c6be3154eb1e9c0
ur-fault <minefuf.fufik@gmail.com>
2023-08-13 13:47:24 +0200

    python_unreadable: add comment

--- python_unreadable/src/main.py
+++ python_unreadable/src/main.py
@@ -1,1 +1,1 @@
-for n in range((100,ds:={3:"Fizz",5:"Buzz",7:"Bazz"})[0]):s='';[(s:=s+w)for d,w in ds.items()if n%d==0];print(s or n)
+for n in range((100,ds:={3:"Fizz",5:"Buzz",7:"Bazz"})[0]):s='';[(s:=s+w)for d,w in ds.items()if n%d==0];print(s or n) # you can add more words by adding more values to the dictionary
